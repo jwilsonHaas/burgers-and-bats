0a350dbac0038d7841ab126527e348e07b9b6c6c
Jeff Wilson <11215501+jwilsonHaas@users.noreply.github.com>
2020-03-06 07:55:19 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,86 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.disintegrate, 200)
+    mySprite.say("YUM :)", 500)
+    info.changeScoreBy(10)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    mySprite.startEffect(effects.warmRadial, 200)
+    mySprite.say("Ouch :(", 500)
+    info.changeScoreBy(-1)
+})
+info.onCountdownEnd(function () {
+    if (info.score() >= 100) {
+        game.over(true, effects.smiles)
+    } else {
+        game.over(false, effects.melt)
+    }
+})
+let projectile: Sprite = null
+let Burger: Sprite = null
+let mySprite: Sprite = null
+scene.setBackgroundColor(13)
+mySprite = sprites.create(img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
+info.startCountdown(30)
+info.changeScoreBy(10)
+game.onUpdateInterval(3000, function () {
+    Burger = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Food)
+    Burger.setPosition(Math.randomRange(0, 100), Math.randomRange(0, 100))
+    Burger.setVelocity(Math.randomRange(-50, 50), Math.randomRange(-50, 50))
+    Burger.setFlag(SpriteFlag.BounceOnWall, true)
+})
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSide(img`
+. . f f f . . . . . . . . f f f 
+. f f c c . . . . . . f c b b c 
+f f c c . . . . . . f c b b c . 
+f c f c . . . . . . f b c c c . 
+f f f c c . c c . f c b b c c . 
+f f c 3 c c 3 c c f b c b b c . 
+f f b 3 b c 3 b c f b c c b c . 
+. c 1 b b b 1 b c b b c c c . . 
+. c 1 b b b 1 b b c c c c . . . 
+c b b b b b b b b b c c . . . . 
+c b 1 f f 1 c b b b b f . . . . 
+f f 1 f f 1 f b b b b f c . . . 
+f f 2 2 2 2 f b b b b f c c . . 
+. f 2 2 2 2 b b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+`, Math.randomRange(-50, 50), Math.randomRange(-50, 50))
+})
